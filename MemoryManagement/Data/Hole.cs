c15043fd4da25a30382516fe410a8c96cf35ccca
DoaaOsama <doaa_kandeel@hotmail.com>
2016-05-03 19:55:42 +0200

    LatestChanges

--- MemoryManagement/Data/Hole.cs
+++ MemoryManagement/Data/Hole.cs
@@ -2,16 +2,11 @@
 {
     internal class Hole
     {
-        public uint id { get; }
         public uint BaseReg { get; }
         
         public uint Size { get; set; }
-        private static uint _counter = 0;
         public Hole(uint baseReg, uint size)
         {
-            _counter++;
-            id = _counter;
-            
             this.BaseReg = baseReg;
             this.Size = size;
 
